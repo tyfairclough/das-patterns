d7352d4709296a2f2e0962483309f6d99696a80d
Ty Fairclough <ty.fairclough@gmail.com>
2017-04-25 09:40:25 +0100

    fix routes

--- app/routes.js
+++ app/routes.js
@@ -7,19 +7,19 @@ module.exports = {
       res.render(req.params.page)
     })
     app.get('/:p1/:p2', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2, {'sprint' : req.params.p2})
+      res.render(req.params.p1 + "/" + req.params.p2)
     })
     app.get('/:p1/:p2/:p3', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3, {'sprint' : req.params.p2})
+      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3)
     })
     app.get('/:p1/:p2/:p3/:p4', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4, {'sprint' : req.params.p2})
+      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4)
     })
     app.get('/:p1/:p2/:p3/:p4/:p5', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5, {'sprint' : req.params.p2})
+      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5)
     })
     app.get('/:p1/:p2/:p3/:p4/:p5/:p6', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5 + "/" + req.params.p6, {'sprint' : req.params.p2})
+      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5 + "/" + req.params.p6)
     })
     app.get('/admin', function (req, res) {
             res.send('admin')
