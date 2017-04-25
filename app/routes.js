e8501cf4a1b54d17173232963436fe15bd5b048e
James King <hello@jamesmichaelking.com>
2017-04-25 11:00:38 +0100

    NODE Modules

--- app/routes.js
+++ app/routes.js
@@ -1,29 +1,25 @@
 module.exports = {
  bind: function (app, assetPath) {
    app.get('/', function (req, res) {
-      res.send('root')
+      res.render('patterns/index')
     })
     app.get('/:p1', function (req, res) {
-      res.render(req.params.page)
+      res.render('patterns', req.params.page)
     })
     app.get('/:p1/:p2', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2)
+      res.render("patterns/" + req.params.p1 + "/" + req.params.p2)
     })
     app.get('/:p1/:p2/:p3', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3)
+      res.render("patterns/" + req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3)
     })
     app.get('/:p1/:p2/:p3/:p4', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4)
+      res.render("patterns/" + req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4)
     })
     app.get('/:p1/:p2/:p3/:p4/:p5', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5)
+      res.render("patterns/" + req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5)
     })
     app.get('/:p1/:p2/:p3/:p4/:p5/:p6', function (req, res) {
-      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5 + "/" + req.params.p6)
-    })
-    app.get('/admin', function (req, res) {
-            res.send('admin')
-//res.render(req.params.page)
+      res.render("patterns/" + req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5 + "/" + req.params.p6)
     })
  }
 }
